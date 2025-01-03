d76edab3c33c1b29cc3b705587a0f4687b94c430
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-01-03 11:01:38 +0300

    Ders 27

---
+++ CSharpEgitimKampi601/FrmEmployee.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace CSharpEgitimKampi601
+{
+    partial class FrmEmployee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnGetById = new System.Windows.Forms.Button();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.txtEmployeeSalary = new System.Windows.Forms.TextBox();
+            this.lblMusteriSehir = new System.Windows.Forms.Label();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.lblMusteriAd = new System.Windows.Forms.Label();
+            this.txtEmployeeSurname = new System.Windows.Forms.TextBox();
+            this.lblMusteriSoyad = new System.Windows.Forms.Label();
+            this.txtEmployeeId = new System.Windows.Forms.TextBox();
+            this.lblMusteriId = new System.Windows.Forms.Label();
+            this.txtDepartment = new System.Windows.Forms.Label();
+            this.cmbEmployeeDepartment = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Location = new System.Drawing.Point(386, 21);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.RowHeadersWidth = 51;
+            this.DataGridView1.RowTemplate.Height = 24;
+            this.DataGridView1.Size = new System.Drawing.Size(700, 346);
+            this.DataGridView1.TabIndex = 50;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEkle.Location = new System.Drawing.Point(156, 205);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(200, 36);
+            this.btnEkle.TabIndex = 49;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSil.Location = new System.Drawing.Point(156, 247);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(200, 36);
+            this.btnSil.TabIndex = 48;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = false;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGuncelle.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGuncelle.Location = new System.Drawing.Point(156, 289);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(200, 36);
+            this.btnGuncelle.TabIndex = 47;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = false;
+            // 
+            // btnGetById
+            // 
+            this.btnGetById.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnGetById.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGetById.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGetById.Location = new System.Drawing.Point(156, 331);
+            this.btnGetById.Name = "btnGetById";
+            this.btnGetById.Size = new System.Drawing.Size(200, 36);
+            this.btnGetById.TabIndex = 46;
+            this.btnGetById.Text = "ID\'ye Göre Getir";
+            this.btnGetById.UseVisualStyleBackColor = false;
+            // 
+            // btnListele
+            // 
+            this.btnListele.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnListele.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnListele.Location = new System.Drawing.Point(156, 164);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(200, 36);
+            this.btnListele.TabIndex = 45;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = false;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // txtEmployeeSalary
+            // 
+            this.txtEmployeeSalary.Location = new System.Drawing.Point(156, 105);
+            this.txtEmployeeSalary.Name = "txtEmployeeSalary";
+            this.txtEmployeeSalary.Size = new System.Drawing.Size(200, 22);
+            this.txtEmployeeSalary.TabIndex = 44;
+            // 
+            // lblMusteriSehir
+            // 
+            this.lblMusteriSehir.AutoSize = true;
+            this.lblMusteriSehir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMusteriSehir.Location = new System.Drawing.Point(90, 107);
+            this.lblMusteriSehir.Name = "lblMusteriSehir";
+            this.lblMusteriSehir.Size = new System.Drawing.Size(60, 20);
+            this.lblMusteriSehir.TabIndex = 43;
+            this.lblMusteriSehir.Text = "Maaş:";
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.Location = new System.Drawing.Point(156, 49);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(200, 22);
+            this.txtEmployeeName.TabIndex = 42;
+            // 
+            // lblMusteriAd
+            // 
+            this.lblMusteriAd.AutoSize = true;
+            this.lblMusteriAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMusteriAd.Location = new System.Drawing.Point(113, 51);
+            this.lblMusteriAd.Name = "lblMusteriAd";
+            this.lblMusteriAd.Size = new System.Drawing.Size(37, 20);
+            this.lblMusteriAd.TabIndex = 41;
+            this.lblMusteriAd.Text = "Ad:";
+            // 
+            // txtEmployeeSurname
+            // 
+            this.txtEmployeeSurname.Location = new System.Drawing.Point(156, 77);
+            this.txtEmployeeSurname.Name = "txtEmployeeSurname";
+            this.txtEmployeeSurname.Size = new System.Drawing.Size(200, 22);
+            this.txtEmployeeSurname.TabIndex = 40;
+            // 
+            // lblMusteriSoyad
+            // 
+            this.lblMusteriSoyad.AutoSize = true;
+            this.lblMusteriSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMusteriSoyad.Location = new System.Drawing.Point(84, 79);
+            this.lblMusteriSoyad.Name = "lblMusteriSoyad";
+            this.lblMusteriSoyad.Size = new System.Drawing.Size(66, 20);
+            this.lblMusteriSoyad.TabIndex = 39;
+            this.lblMusteriSoyad.Text = "Soyad:";
+            // 
+            // txtEmployeeId
+            // 
+            this.txtEmployeeId.Location = new System.Drawing.Point(156, 21);
+            this.txtEmployeeId.Name = "txtEmployeeId";
+            this.txtEmployeeId.Size = new System.Drawing.Size(200, 22);
+            this.txtEmployeeId.TabIndex = 38;
+            // 
+            // lblMusteriId
+            // 
+            this.lblMusteriId.AutoSize = true;
+            this.lblMusteriId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMusteriId.Location = new System.Drawing.Point(116, 23);
+            this.lblMusteriId.Name = "lblMusteriId";
+            this.lblMusteriId.Size = new System.Drawing.Size(34, 20);
+            this.lblMusteriId.TabIndex = 37;
+            this.lblMusteriId.Text = "ID:";
+            // 
+            // txtDepartment
+            // 
+            this.txtDepartment.AutoSize = true;
+            this.txtDepartment.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtDepartment.Location = new System.Drawing.Point(43, 135);
+            this.txtDepartment.Name = "txtDepartment";
+            this.txtDepartment.Size = new System.Drawing.Size(107, 20);
+            this.txtDepartment.TabIndex = 51;
+            this.txtDepartment.Text = "Departman:";
+            // 
+            // cmbEmployeeDepartment
+            // 
+            this.cmbEmployeeDepartment.FormattingEnabled = true;
+            this.cmbEmployeeDepartment.Location = new System.Drawing.Point(157, 135);
+            this.cmbEmployeeDepartment.Name = "cmbEmployeeDepartment";
+            this.cmbEmployeeDepartment.Size = new System.Drawing.Size(199, 24);
+            this.cmbEmployeeDepartment.TabIndex = 52;
+            // 
+            // FrmEmployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 376);
+            this.Controls.Add(this.cmbEmployeeDepartment);
+            this.Controls.Add(this.txtDepartment);
+            this.Controls.Add(this.DataGridView1);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnGetById);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.txtEmployeeSalary);
+            this.Controls.Add(this.lblMusteriSehir);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.lblMusteriAd);
+            this.Controls.Add(this.txtEmployeeSurname);
+            this.Controls.Add(this.lblMusteriSoyad);
+            this.Controls.Add(this.txtEmployeeId);
+            this.Controls.Add(this.lblMusteriId);
+            this.Name = "FrmEmployee";
+            this.Text = "FrmEmployee";
+            this.Load += new System.EventHandler(this.FrmEmployee_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DataGridView1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnGetById;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.TextBox txtEmployeeSalary;
+        private System.Windows.Forms.Label lblMusteriSehir;
+        private System.Windows.Forms.TextBox txtEmployeeName;
+        private System.Windows.Forms.Label lblMusteriAd;
+        private System.Windows.Forms.TextBox txtEmployeeSurname;
+        private System.Windows.Forms.Label lblMusteriSoyad;
+        private System.Windows.Forms.TextBox txtEmployeeId;
+        private System.Windows.Forms.Label lblMusteriId;
+        private System.Windows.Forms.Label txtDepartment;
+        private System.Windows.Forms.ComboBox cmbEmployeeDepartment;
+    }
+}
